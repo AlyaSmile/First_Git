00be48f614f7a24854f66217df5fbbebaa129b93
AlyaSmile <alyasailor@yandex.ru>
2017-06-11 12:55:48 +0300

    Class Diagram

---
+++ ConsoleAppTest/ConsoleAppTest/BMV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTest
+{
+    public class BMV : Car
+    {
+        public override string Color { get; set; }
+        public override string Name { get;set; }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"{Name}, {Color}");
+        }
+    }
+}
---
+++ ConsoleAppTest/ConsoleAppTest/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTest
+{
+    public abstract class Car
+    {
+        public abstract string Color
+        {
+            get;
+            set;
+        }
+
+        public abstract string Name
+        {
+            get;
+            set;
+        }
+
+        public abstract void PrintInfo();
+
+    }
+}
