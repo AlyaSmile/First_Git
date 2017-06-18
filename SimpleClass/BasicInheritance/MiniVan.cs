b813205465c7ce2da171a21b7b574adf56a55520
AlyaSmile <alyasailor@yandex.ru>
2017-06-18 23:51:18 +0300

    Class * наследование

---
+++ SimpleClass/BasicInheritance/MiniVan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    sealed class MiniVan:Car
+    {
+        public void TestMethod()
+        {
+            // OK! Can access public members
+            // of a parent within a derived type.
+            Speed = 10;
+            // Ошибка, нет доступа к закрытому члену!
+            // Доступен только в базовом
+            //currSpeed = 10;
+        }
+    }
+}
