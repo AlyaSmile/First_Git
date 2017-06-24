47510d62c7477d2c1c7e01b5e790cfee117fd48f
AlyaSmile <alyasailor@yandex.ru>
2017-06-24 23:56:29 +0300

    Party * первое

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage = "Имя должно быть заполнено!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email должнен быть заполнен!")]
+        [RegularExpression (".+\\@.+\\..+",ErrorMessage ="Введите корректный Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Введите телефон!")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Введите ждать ли вас!")]
+        public bool? WillAttend { get; set; }
+    }
+}
